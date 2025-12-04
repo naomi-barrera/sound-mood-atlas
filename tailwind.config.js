c610c39fe65cee588d5dc5b9a89c93d268969529
Naomi Barrera <naomi@naomibarrera.com>
2025-12-03 23:08:45 -0800

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        ink: "#0b0b0f",
+      },
+    },
+  },
+  plugins: [],
+};
